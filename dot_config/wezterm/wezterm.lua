89d50b554272a3e2190ea31cec9603dcd74268c6
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-01-02 21:31:16 -0600

    fix: remove commented out CaskaydiaCove Nerd Font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder() 
 config.color_scheme = 'Dracula (Official)'
---config.font = wezterm.font 'CaskaydiaCove Nerd Font'
 config.font = wezterm.font 'MonoLisa Nerd Font'
 config.default_prog = { 'pwsh', '-Login', '-NoExit' }
 config.keys = {
