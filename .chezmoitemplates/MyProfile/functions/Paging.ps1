0ae78910a6517035f02e58ad2b197e3362ed02d4
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-01-14 00:14:51 -0600

    feat: add format-json

--- .chezmoitemplates/MyProfile/functions/Paging.ps1
+++ .chezmoitemplates/MyProfile/functions/Paging.ps1
@@ -237,6 +237,31 @@ function Format-Sql {
     }
 }
 
+function Format-Json {
+    [Alias('fjson')]
+    [CmdletBinding(PositionalBinding = $false)]
+    [OutputType([string])]
+    param(
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]] $ArgumentList,
+
+        [Parameter(ValueFromPipeline)]
+        [string] $InputObject
+    )
+    begin {
+        if ($ArgumentList.Count -eq 0 -or $null -eq $ArgumentList) {
+            $ArgumentList = '.'
+        }
+        $pipe = { Out-Jq @ArgumentList | Out-Bat }.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $pipe.Begin($PSCmdlet)
+    }
+    process {
+        $pipe.Process($PSItem)
+    }
+    end {
+        $pipe.End()
+    }
+}
 function Format-Xml {
     #.Synopsis
     #   Pretty-print formatted XML source
