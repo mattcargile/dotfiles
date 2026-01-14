25aae5f769be16d83e58b316a2809119d7e84cee
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-01-14 00:14:29 -0600

    fix: fxml add try/catch for disposing. other cleanup

--- .chezmoitemplates/MyProfile/functions/Paging.ps1
+++ .chezmoitemplates/MyProfile/functions/Paging.ps1
@@ -257,38 +257,50 @@ function Format-Xml {
     [CmdletBinding()]
     param(
         #   The Xml Document
-        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "Document")]
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline, ParameterSetName = "Document")]
         [xml]$Xml,
 
         # The path to an xml document (on disc or any other content provider).
-        [Parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "File")]
+        [Parameter(Position = 0, Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = "File")]
         [Alias("PsPath")]
         [string]$Path,
 
         # The indent level (defaults to 2 spaces)
-        [Parameter(Mandatory = $false)]
         [int]$Indent = 2,
 
         # The indent character (defaults to a space)
         [char]$Character = ' '
     )
     begin {
-        $ArgumentList += '--language', 'xml'
-        $pipe = { Out-Bat @ArgumentList }.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $batArgs = '--language', 'xml'
+        $pipe = { Out-Bat @batArgs }.GetSteppablePipeline($MyInvocation.CommandOrigin)
         $pipe.Begin($PSCmdlet)
     }
     process {
-        if ($Path) { [xml]$xml = Get-Content $Path }
 
-        $StringWriter = New-Object System.IO.StringWriter
-        $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter
-        $xmlWriter.Formatting = "indented"
-        $xmlWriter.Indentation = $Indent
-        $xmlWriter.IndentChar = $Character
-        $xml.WriteContentTo($XmlWriter)
-        $XmlWriter.Flush()
-        $StringWriter.Flush()
-        $pipe.Process($StringWriter.ToString())
+        try {
+            if ($Path) {
+                $Xml = Get-Content $Path -Raw
+            }
+            $stringWriter = New-Object System.IO.StringWriter
+            $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter
+            $xmlWriter.Formatting = [System.Xml.Formatting]::Indented
+            $xmlWriter.Indentation = $Indent
+            $xmlWriter.IndentChar = $Character
+            $Xml.WriteContentTo($XmlWriter)
+            $pipe.Process($stringWriter.ToString())
+        }
+        catch {
+            throw
+        }
+        finally {
+            if ($xmlWriter) {
+                $xmlWriter.Dispose()
+            }
+            if ($stringWriter) {
+                $stringWriter.Dispose()
+            }
+        }
     }
     end {
         $pipe.End()
