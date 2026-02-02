3b424ba603fe7c367e175b1a2b7d0757e38fe7e8
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-02-02 13:26:17 -0600

    feat: prompt template homebrew shell check

--- .chezmoitemplates/prompt.ps1
+++ .chezmoitemplates/prompt.ps1
@@ -3,6 +3,9 @@
 # executiontime postfix invisible spacing character for bug in wt.exe (https://github.com/JanDeDobbeleer/oh-my-posh/discussions/668)
 # Had to change the hourglass icon
 $ompConfigPath = Join-Path -Path $HOME -ChildPath .config | Join-Path -ChildPath 'oh-my-posh' | Join-Path -ChildPath 'night-owl_mac.omp.json'
+# chezmoi HomeBrew Shell Env if block {{ if lookPath "brew" }}
+& $env:HOMEBREW_PREFIX/bin/brew shellenv | Invoke-Expression
+# end {{ end }}
 oh-my-posh init pwsh --config $ompConfigPath | Invoke-Expression
 
 # Custom OMP Prompt Context
