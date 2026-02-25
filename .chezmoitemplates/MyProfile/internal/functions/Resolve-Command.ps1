0744b3b35149eab5667a44f20f65e2c056c7ba7a
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-02-24 18:16:16 -0600

    fix: gcp only support alias, filter, function, cmdlet

--- .chezmoitemplates/MyProfile/internal/functions/Resolve-Command.ps1
+++ .chezmoitemplates/MyProfile/internal/functions/Resolve-Command.ps1
@@ -10,10 +10,11 @@ function Resolve-Command {
         if (-not $Command) {
             return $null
         }
+        $supportedCommandTypes = 'Alias', 'Filter', 'Cmdlet', 'Function'
         # Need `Get-Command` to auto load module. using `GetCommand` doesn't 
         $cmd = $Command
         if ($cmd -is [string]) {
-            $cmd = Get-Command -Name ([WildcardPattern]::Escape($cmd)) 
+            $cmd = Get-Command -Name ([WildcardPattern]::Escape($cmd)) -CommandType $supportedCommandTypes
             if (-not $cmd) { $cmd = $inputData }
         }
         # Some commands have pre-loaded modules so they resolve properly.
@@ -26,13 +27,13 @@ function Resolve-Command {
             # Some fully qualified module and cmdlet names return the wrong ModuleName and Source like `Get-ScheduledTask`
             $cmdDefEscSplit = $cmdDefEsc -split '\\', 2
             if ($cmdDefEscSplit.Count -eq 2) {
-                $cmd = Get-Command -Module $cmdDefEscSplit[0] -Name $cmdDefEscSplit[1]
+                $cmd = Get-Command -Module $cmdDefEscSplit[0] -Name $cmdDefEscSplit[1] -CommandType $supportedCommandTypes
                 if (-not $cmd) {
-                    $cmd = Get-Command -Name $cmdDefEscSplit[1]
+                    $cmd = Get-Command -Name $cmdDefEscSplit[1] -CommandType $supportedCommandTypes
                 }
             }
             else {
-                $cmd = Get-Command $cmdDefEsc 
+                $cmd = Get-Command $cmdDefEsc -CommandType $supportedCommandTypes
             }
         }
         if ($cmd -is [System.Management.Automation.CommandInfo]) {
