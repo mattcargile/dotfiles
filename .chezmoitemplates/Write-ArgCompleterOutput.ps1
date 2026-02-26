50fbc6bfce3f4042a92e7e89ba4b8bc5a395b1bb
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-02-25 21:34:52 -0600

    feat: new carapacge op 1password exclusion due to op alias on pager

--- .chezmoitemplates/Write-ArgCompleterOutput.ps1
+++ .chezmoitemplates/Write-ArgCompleterOutput.ps1
@@ -45,7 +45,7 @@ end {
     #region carapace completers first.
     if (Get-Command carapace -CommandType Application -ErrorAction Ignore) {
         $env:CARAPACE_ENV = 0 # Don't add environment helper functions
-        $env:CARAPACE_EXCLUDES = 'ls,bat,rg,fd,gh,chezmoi,glow,bb,dotnet,winget,get-env,set-env,unset-env,ov,sudo,cp,ps,cat,rm,mv,mkdir,code,code-insiders' # Exclude completions that conflict or already exist from software creator.
+        $env:CARAPACE_EXCLUDES = 'ls,bat,rg,fd,gh,chezmoi,glow,bb,dotnet,winget,get-env,set-env,unset-env,ov,sudo,cp,ps,cat,rm,mv,mkdir,code,code-insiders,op' # Exclude completions that conflict or already exist from software creator.
         # Need to add this to the path before running script because there is logic in the script to add this to the process. Need to make the script more consistent
         $env:Path += ";$env:APPDATA\carapace\bin" -replace '\\', '/' # carapace golang binary uses forward slashes to check for path
         $argCompFiles.Add( ( New-ArgCompleterObject -Script (carapace _carapace powershell | Out-String) -Comment 'Carapace Various Completions' ) )
