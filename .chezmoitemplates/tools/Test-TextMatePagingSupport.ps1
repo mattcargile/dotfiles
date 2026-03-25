e01105510500cb28195fb84494b6c7ab06f5e90c
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-03-25 04:42:25 -0500

    feat: fcs, fpsh, fsql, textmate pager handling with template

--- .chezmoitemplates/tools/Test-TextMatePagingSupport.ps1
+++ .chezmoitemplates/tools/Test-TextMatePagingSupport.ps1
@@ -6,10 +6,11 @@ param (
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
+$pwshSupportedVersion = [version]'7.4.0'
 $pagerSupportedVersion = [version]'0.2.1'
 $latestModuleVersion = ( Get-Module -Name TextMate -ListAvailable | Sort-Object -Property Version -Descending -Top 1 ).Version
 
-if ($latestModuleVersion -ge $pagerSupportedVersion) {
+if ($PSVersionTable.PSVersion -ge $pwshSupportedVersion -and $latestModuleVersion -ge $pagerSupportedVersion) {
     $true.ToString()
 }
 else {
