5182bf7b86455329b823ca3da355804b13b2c3da
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-03-29 18:40:21 -0500

    fix: test text mate handling if textmate module doesn't exist

--- .chezmoitemplates/tools/Test-TextMatePagingSupport.ps1
+++ .chezmoitemplates/tools/Test-TextMatePagingSupport.ps1
@@ -8,7 +8,7 @@ Set-StrictMode -Version Latest
 
 $pwshSupportedVersion = [version]'7.4.0'
 $pagerSupportedVersion = [version]'0.2.1'
-$latestModuleVersion = ( Get-Module -Name TextMate -ListAvailable | Sort-Object -Property Version -Descending -Top 1 ).Version
+$latestModuleVersion = Get-Module -Name TextMate -ListAvailable | Sort-Object -Property Version -Descending -Top 1 | ForEach-Object -MemberName Version
 
 if ($PSVersionTable.PSVersion -ge $pwshSupportedVersion -and $latestModuleVersion -ge $pagerSupportedVersion) {
     $true.ToString()
