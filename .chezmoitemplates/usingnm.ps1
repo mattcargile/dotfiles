aef5035467ef6be6b88a71bb0e959ec271674edc
Matthew A Cargile <74641867+mattcargile@users.noreply.github.com>
2026-04-01 12:03:52 -0500

    feat: add specialized namespace

--- .chezmoitemplates/usingnm.ps1
+++ .chezmoitemplates/usingnm.ps1
@@ -1,6 +1,7 @@
 using namespace System
 using namespace System.Collections
 using namespace System.Collections.Generic
+using namespace System.Collections.Specialized
 using namespace System.ComponentModel
 using namespace System.Data
 using namespace System.Data.OleDb
